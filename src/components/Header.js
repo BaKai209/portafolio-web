8457a83d5c27a420b7eba853507b874d8dcb8582
bakai209 <ss4.sell@gmail.com>
2022-06-08 15:03:54 -0500

    creacion del Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,56 @@
+import { Close, MenuOutlined } from '@material-ui/icons'
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Header.scss'
 
 const Header = () => {
   return (
     <div className='header'>
+
       <div className='header__logo'>
         <h1>BaKai209</h1>
+      </div>
+      
+        <nav className='navbar'>
 
-        <nav>
           <ul>
+
             <div className="closed">
-              
+              <Close className='close' />
             </div>
+
+            <li>
+              <Link to='/'>home</Link>
+            </li>
+
+            <li>
+              <Link to='/'>About Me</Link>
+            </li>
+
+            <li>
+              <Link to='/'>Portfolio</Link>
+            </li>
+
+            <li>
+              <Link to='/'>Pages</Link>
+            </li>
+
+            <li>
+              <Link to='/'>Blog</Link>
+            </li>
+
+            <li>
+              <Link to='/'>Contact Me</Link>
+            </li>
+
           </ul>
+
         </nav>
 
-      </div>
+        <div className="changer">
+        <MenuOutlined className='menu'/>       
+        </div>
+
 
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
   <Router>
+    <Routes>
     <Route path='/' element={<App/>}/>
+    </Routes>
   </Router>,
   document.getElementById('root')
 );
